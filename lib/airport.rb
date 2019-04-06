6b7274d240bab8e1baba482130566af6569cde08
Maria Vanaale <mvanaale@gmail.com>
2019-04-06 16:26:32 +0100

    checks plane is not in the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,13 @@
+require 'plane'
+
 class Airport
   def land_plane
   end 
 
   def takeoff_plane
+  end
+
+  def has_plane?(plane)
+    false 
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,4 +9,10 @@ describe Airport do
   it 'plane can take off' do
     expect(subject).to respond_to(:takeoff_plane)
   end
+
+  it 'able to confirm plane no longer in the airtport' do 
+    plane = Plane.new
+    airport = Airport.new
+    expect(airport.has_plane?(plane)).to eq false
+  end 
 end
