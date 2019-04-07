2267eff85479321fc14b2dd8786f5e3298f147b6
Maria Vanaale <mvanaale@gmail.com>
2019-04-07 11:06:04 +0100

    create global variable for plane to make each test less than 3 lines

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,13 +2,17 @@ require './lib/airport'
 require './lib/plane'
 
 describe Airport do
+
+  before (:each) do
+    @plane = Plane.new
+  end
+
   it 'can land a plane' do
     expect(subject).to respond_to(:land)
   end
 
   it 'can confirm plane is in the airport' do
-    plane = Plane.new
-    expect(subject.land(plane)).to eq [plane]
+    expect(subject.land(@plane)).to eq [@plane]
   end
 
   it 'plane can take off' do
@@ -16,10 +20,9 @@ describe Airport do
   end
 
   it 'able to confirm plane no longer in the airtport' do 
-    plane = Plane.new
-    subject.land(plane)
-    plane = subject.takeoff(plane)
-    expect(subject.hangar).not_to include plane
+    subject.land(@plane)
+    plane = subject.takeoff(@plane)
+    expect(subject.hangar).not_to include @plane
   end 
 
   it 'has a default capacity of 10' do 
@@ -29,14 +32,12 @@ describe Airport do
 
   it 'prevents plane from landing if airport is full' do
     airport = Airport.new
-    plane = Plane.new
-    Airport::DEFAULT_CAPACITY.times { airport.land(plane)}
-    expect { airport.land(plane) }.to raise_error 'Airport Full!'
+    Airport::DEFAULT_CAPACITY.times { airport.land(@plane)}
+    expect { airport.land(@plane) }.to raise_error 'Airport Full!'
   end 
 
   it 'prevents plane from taking off if the weather is stormy' do
-    plane = Plane.new
     allow(subject).to receive(:stormy?).and_return true
-    expect { subject.takeoff(plane) }.to raise_error 'Weather is stormy!No take off'
+    expect { subject.takeoff(@plane) }.to raise_error 'Weather is stormy!No take off'
   end
 end
